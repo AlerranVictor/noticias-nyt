80528f5610c9ead2b4f038b34ce7d9d581c54a6d
Alerran Victor <alerransilva13@gmail.com>
2023-08-19 21:08:44 -0300

    barras

--- src/app/components/cabecalho/cabecalho.component.ts
+++ src/app/components/cabecalho/cabecalho.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Renderer2 } from '@angular/core';
 
 @Component({
   selector: 'app-cabecalho',
@@ -6,5 +6,25 @@ import { Component } from '@angular/core';
   styleUrls: ['./cabecalho.component.css']
 })
 export class CabecalhoComponent {
+
+  constructor(private renderer: Renderer2) { }
+
+  mostrarMenu(){
+    let control = 0
+    const barras = document.getElementById('barras')
+    const barraOpen = document.getElementById('barraOpen')
+
+    if (control === 0){
+      this.renderer.removeClass(barraOpen, 'menu-invisivel')
+      this.renderer.addClass(barras, 'menu-visivel')
+      
+      control += 1
+    }
+    else {
+      this.renderer.removeClass(barras, 'menu-visivel')
+      this.renderer.addClass(barras, 'menu-invisivel')
+      control -= 1
+    }
+  }
 
 }
